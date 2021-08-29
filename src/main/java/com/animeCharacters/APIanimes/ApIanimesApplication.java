7ec939efa7409b6a24d32f9d10cbf33d02a31c71
EdelbertoRosler <edelbertorosler@gmail.com>
2021-08-29 10:08:37 -0300

    sending initial files generated in Spring Boot

---
+++ src/main/java/com/animeCharacters/APIanimes/ApIanimesApplication.java
@@ -0,0 +1,13 @@
+package com.animeCharacters.APIanimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIanimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIanimesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animeCharacters/APIanimes/ApIanimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animeCharacters.APIanimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIanimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
